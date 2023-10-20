509501e8ae18e2b769a80979181f92733cee880d
PROF ROMULO <rjlpena@gmail.com>
2023-10-20 10:42:16 -0300

    função tocasompom

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+//Declaração/criação da função tocaSomPom
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
-
+//chamada ou invocação da função tocaSomPom ao clicar no botão
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 
