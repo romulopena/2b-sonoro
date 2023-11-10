51e4174fb8dca3740fdcb71dd8c5d0e1f9daaf89
PROF ROMULO <rjlpena@gmail.com>
2023-11-10 11:05:30 -0300

    comentarios

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ function tocaSom(idElementoAudio){
 }
 //criando a referências constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
+//criação da repetição "while" trocado pelo "for" com variavel,condição e incremento
 for(let contador = 0; contador < listaDeTeclas.length; contador++){
-    const teclas = listaDeTeclas[contador];
-    const efeito = teclas.classList[1];
-    const idAudio = `#som_${efeito}`;
-    teclas.onclick = function(){
-        tocaSom(idAudio);
+    const teclas = listaDeTeclas[contador]; //guarda o contador da lista
+    const efeito = teclas.classList[1];  //chama o 2º item classe individual do som
+    const idAudio = `#som_${efeito}`;  //utiliza a junção template string ${ }
+    teclas.onclick = function(){      //função anônima auxiliar para chamar tocaSom
+        tocaSom(idAudio);        //chamada da função tocaSom com parâmetro idAudio
     }
 }
