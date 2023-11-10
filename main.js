7c24ed62b517f3f26748232ad26cc3e8e23c4e56
PROF ROMULO <rjlpena@gmail.com>
2023-11-10 11:00:45 -0300

    template string + laço FOR

--- main.js
+++ main.js
@@ -4,14 +4,12 @@ function tocaSom(idElementoAudio){
 }
 //criando a referências constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
-while(contador < listaDeTeclas.length){
-    const teclas = listaDeTeclas[contador]
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const teclas = listaDeTeclas[contador];
     const efeito = teclas.classList[1];
     const idAudio = `#som_${efeito}`;
     teclas.onclick = function(){
         tocaSom(idAudio);
     }
-    contador = contador + 1;
-    console.log(contador);
 }
