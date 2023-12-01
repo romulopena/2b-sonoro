cb1ef52cc4590d286602cd63563b10a7c86bdbf4
PROF ROMULO <rjlpena@gmail.com>
2023-12-01 13:56:00 -0300

    ok

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-//Declaração/criação da função 
 function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
-//criando a referências constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
-//criação da repetição "while" trocado pelo "for" com variavel,condição e incremento
+
 for(let contador = 0; contador < listaDeTeclas.length; contador++){
-    const teclas = listaDeTeclas[contador]; //guarda o contador da lista
-    const efeito = teclas.classList[1];  //chama o 2º item classe individual do som
-    const idAudio = `#som_${efeito}`;  //utiliza a junção template string ${ }
-    teclas.onclick = function(){      //função anônima auxiliar para chamar tocaSom
-        tocaSom(idAudio);        //chamada da função tocaSom com parâmetro idAudio
+    const teclas = listaDeTeclas[contador]; 
+    const efeito = teclas.classList[1];  
+    const idAudio = `#som_${efeito}`;  
+    teclas.onclick = function(){      
+        tocaSom(idAudio);       
     }
     teclas.onkeydown = function(evento){
         if(evento.code != 'Tab'){
@@ -20,5 +18,4 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
     teclas.onkeyup = function(){
         teclas.classList.remove('ativa');
     }
-    
 }
